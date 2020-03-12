db4146547f419d1f5e8fecaa52c50692ac4e8ac8
Mateus Macedo <mhbassomacedo@gmail.com>
2020-03-12 00:08:25 -0300

    Create File Controller and Migrations
    Signed-off-by: Mateus Macedo <mhbassomacedo@gmail.com>

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,15 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
+
 import User from './app/models/User';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
-
+import FileController from './app/controllers/FileController';
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.get('/', async (request, response) => {
   const user = await User.create({
@@ -23,4 +27,5 @@ routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
 
+routes.post('/files', upload.single('file'), FileController.store);
 export default routes;
