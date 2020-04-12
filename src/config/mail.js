fbae0eb3b5d388889320bb6321eae067621fa8b2
Mateus Macedo <mhbassomacedo@gmail.com>
2020-04-11 21:50:04 -0300

    Added Nodemailer to send mail for cancel appointment
    Signed-off-by: Mateus Macedo <mhbassomacedo@gmail.com>

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: 'a130322045b4fc',
+    pass: 'bd416eaa1147d1',
+  },
+  default: {
+    from: 'Equipe GoBarder <noreply@gobarber.com>',
+  },
+};
